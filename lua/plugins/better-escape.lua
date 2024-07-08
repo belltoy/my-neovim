c3190531c9a002bb98bea1d2ed151e06f82a1811
belltoy <belltoy@gmail.com>
2024-07-08 11:55:15 +0800

    Simplify better-escape config

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,11 +1,6 @@
 return {
   'max397574/better-escape.nvim',
   config = function()
-    require('better_escape').setup({
-      mapping = {'jk', 'jj'},
-      timeout = vim.o.timeoutlen,
-      clear_empty_lines = true,
-      keys = '<Esc>',
-    })
+    require("better_escape").setup()
   end
 }
