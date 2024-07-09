569848a4349e2c70ce58e003d867bb44c1852df3
belltoy <belltoy@gmail.com>
2024-07-09 23:18:26 +0800

    Remove unused codes

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -8,35 +8,25 @@ local servers = {
   'bufls',
   'jqls',
 
-  -- 'denols',
-
-  'ltex',
+  -- 'ltex',
 
   -- python
-  -- 'debugpy',
-  -- 'black',
-  -- 'ruff',
 
   'gopls',
   'html',
   'jsonls',
   'jdtls', -- Java
   'marksman', -- Markdown
-  -- 'sumneko_lua',
   'lua_ls',
-  -- 'sourcekit', -- Swift
-  -- 'rust_analyzer',
   'tsserver',
   -- 'pyright',
   'yamlls',
   'bashls',
   'taplo', -- Toml
-  -- 'erlangls',
-  'elp',
   'elixirls',
 }
 
-local on_attach = require('user.mason_settings').on_attach
+-- local on_attach = require('user.mason_settings').on_attach
 
 return {
   'williamboman/mason.nvim', -- simple to use language server installer, replaced williamboman/nvim-lsp-installer
@@ -66,22 +56,44 @@ return {
       ensure_installed = servers,
 
       -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
+      automatic_installation = {
+        exclude = {
+          "elp", "ltex"
+        },
+      }, -- not the same as ensure_installed
     })
 
+    mason_lspconfig.setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function (server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {}
+        end,
+        -- Next, you can provide a dedicated handler for specific servers.
+        -- For example, a handler override for the `rust_analyzer`:
+        -- ["rust_analyzer"] = function ()
+        --     require("rust-tools").setup {}
+        -- end
+    }
+
     for _, server in pairs(servers) do
-        local opts = {
-            on_attach = on_attach,
-            -- capabilities = require('user.lsp.handlers').capabilities,
-        }
+      local opts = {
+        -- on_attach = on_attach,
+        -- capabilities = require('user.lsp.handlers').capabilities,
+      }
 
-        local has_custom_opts, server_custom_opts = pcall(require, 'plugins.lsp.settings.' .. server)
+      local has_custom_opts, server_custom_opts = pcall(require, 'plugins.lsp.settings.' .. server)
 
-        if has_custom_opts then
-            opts = vim.tbl_deep_extend('force', server_custom_opts, opts)
-        end
+      if has_custom_opts then
+        opts = vim.tbl_deep_extend('force', server_custom_opts, opts)
+      end
 
-        lspconfig[server].setup(opts)
+      lspconfig[server].setup(opts)
     end
+
+    -- Manually installed lsp
+    lspconfig['elp'].setup({})
+    lspconfig['ltex'].setup({})
   end
 }
