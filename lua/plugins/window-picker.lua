f78ee716896f6cbc828f55d14234f460274c54f6
belltoy <belltoy@gmail.com>
2024-07-15 13:32:50 +0800

    Customize window-picker

--- lua/plugins/window-picker.lua
+++ lua/plugins/window-picker.lua
@@ -4,6 +4,35 @@ return {
   event = 'VeryLazy',
   version = '2.*',
   config = function()
-    require 'window-picker'.setup()
+    require 'window-picker'.setup({
+      statusline_winbar_picker = {
+        -- You can change the display string in status bar.
+        -- It supports '%' printf style. Such as `return char .. ': %f'` to display
+        -- buffer file path. See :h 'stl' for details.
+        selection_display = function(char, windowid)
+          return '%= ' .. char .. ' %='
+        end,
+
+        -- whether you want to use winbar instead of the statusline
+        -- "always" means to always use winbar,
+        -- "never" means to never use winbar
+        -- "smart" means to use winbar if cmdheight=0 and statusline if cmdheight > 0
+        use_winbar = 'never',     -- "always" | "never" | "smart"
+      },
+      highlights = {
+        statusline = {
+          focused = {
+            fg = '#ededed',
+            bg = '#e35e4f',
+            bold = true,
+          },
+          unfocused = {
+            fg = '#000000',
+            bg = '#ae81ff',
+            bold = true,
+          },
+        },
+      }
+    })
   end,
 }
