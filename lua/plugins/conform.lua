67e5a2793294ffffad6e5396e2cff5e1e2114664
belltoy <belltoy@gmail.com>
2024-07-23 11:15:45 +0800

    Add formatters for conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,20 +8,40 @@ return {
   'stevearc/conform.nvim',
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
+
   keys = {
     { "F",         f,      mode = "v", desc = "Range Format" },
     { "<leader>F", format, mode = "",  desc = "Format buffer" },
   },
+
   opts = {
     format_on_save = nil,
     formatters_by_ft = {
       erlang = { "erlfmt" },
-      rust = { "rustfmt" },
+      rust = { "rustfmt", "injected" },
+      sql = { "sleek" },
+      go = { "gofmt", --[[ "gofumpt", ]] "injected" },
+      yaml = { "yamlfmt", "yq", "injected" },
+      toml = { "taplo", "injected" },
+      xml = { "xmllint" },
+      json = { "jq", "yq" },
+      gleam = { "gleam", "injected" },
 
       -- NOTE: Elixir needs 1.14 or higher
       elixir = { "mix" },
       eelixir = { "mix" },
-    }
+    },
+
+    formatters = {
+      sleek = {
+        command = "sleek",
+        args = { "--uppercase" },
+      },
+      xmllint = {
+        command = "xmllint",
+        args = { "--format", "-" },
+      },
+    },
   },
 
   init = function()
