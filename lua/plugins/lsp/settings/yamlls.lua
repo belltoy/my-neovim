f8d172c86d17f8aa809231b1f1bdaa103beafc17
belltoy <belltoy@gmail.com>
2024-07-23 11:16:01 +0800

    Use old gitlab ci schema

--- lua/plugins/lsp/settings/yamlls.lua
+++ lua/plugins/lsp/settings/yamlls.lua
@@ -1,4 +1,5 @@
-local gitlab_schema = "https://gitlab.com/gitlab-org/gitlab/-/raw/v14.10.5-ee/app/assets/javascripts/editor/schema/ci.json"
+-- local gitlab_schema = "https://gitlab.com/gitlab-org/gitlab/-/raw/v14.10.5-ee/app/assets/javascripts/editor/schema/ci.json"
+local gitlab_schema = "https://gist.githubusercontent.com/belltoy/6e9d0ac4d1edcd533b053fca6e0572fb/raw/198110f01ccee2d0dc8932a5ca2cb880ef8f293f/gitlab-ci.schema.json"
 
 local opts = {
   settings = {
