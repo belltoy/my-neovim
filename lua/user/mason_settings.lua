a4b93cecdaf0dccdf9c90152e215d04c758d168d
belltoy <belltoy@gmail.com>
2024-07-23 11:18:17 +0800

    Update keymap

--- lua/user/mason_settings.lua
+++ lua/user/mason_settings.lua
@@ -1,29 +1,30 @@
-local ext = function(table, key, value)
+local ext = function(table, bufnr, desc)
   local tbl = {}
   for k, v in pairs(table) do
     tbl[k] = v
   end
-  tbl[key] = value
+  tbl["desc"] = desc
+  tbl["buffer"] = bufnr
   return tbl
 end
 
 local lsp_keymaps = function(bufnr)
   local opts = { noremap = true, silent = true, buffer = bufnr}
-  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', ext(opts, "desc", "Hover"))
+  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', ext(opts, bufnr, "Hover"))
   -- conflict with <c-k> for tmux navigation
-  vim.keymap.set('n', '<M-o>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', ext(opts, "desc", "Signature Help"))
+  vim.keymap.set('n', '<M-o>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', ext(opts, bufnr, "Signature Help"))
 
-  vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", ext(opts, "desc", "Rename"))
+  -- vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", ext(opts, bufnr, "Rename"))
 
-  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', ext(opts, "desc", "Goto Declaration"))
-  vim.keymap.set('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', ext(opts, "desc", "Goto Type Definition"))
-  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', ext(opts, "desc", "Goto Definition"))
-  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', ext(opts, "desc", "Goto Implementation"))
-  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', ext(opts, "desc", "Goto References"))
-  vim.keymap.set("n", "gA", "<cmd>lua vim.lsp.buf.code_action()<CR>", ext(opts, "desc", "Code Action"))
-  vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>', ext(opts, "desc", "Open Diagnostic Float"))
-  vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', ext(opts, "desc", "Next Diagnostic"))
-  vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', ext(opts, "desc", "Prev Diagnostic"))
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', ext(opts, bufnr, "Goto Declaration"))
+  vim.keymap.set('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', ext(opts, bufnr, "Goto Type Definition"))
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', ext(opts, bufnr, "Goto Definition"))
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', ext(opts, bufnr, "Goto Implementation"))
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', ext(opts, bufnr, "Goto References"))
+  vim.keymap.set("n", "gA", "<cmd>lua vim.lsp.buf.code_action()<CR>", ext(opts, bufnr, "Code Action"))
+  vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>', ext(opts, bufnr, "Open Diagnostic Float"))
+  vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', ext(opts, bufnr, "Next Diagnostic"))
+  vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', ext(opts, bufnr, "Prev Diagnostic"))
 end
 
 local function lsp_highlight_document(client)
