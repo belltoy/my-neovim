b3062682bf5eab97fd7e406384c0d7a3a390a7ea
belltoy <belltoy@gmail.com>
2024-07-23 11:16:40 +0800

    Fix trouble window and menu

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,7 +3,17 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
   opts = {
     modes = {
-      all = {
+      diagnostics = {
+        mode = "diagnostics",
+        win = {
+          position = "right",
+          size = 80,
+          fixed = false,
+          padding = { top = 0, left = 0 },
+        },
+      },
+
+      s = {
         mode = "symbols",
         win = {
           position = "right",
@@ -13,77 +23,97 @@ return {
         },
       },
 
-      diagnostics_right = {
-        mode = "diagnostics",
+      todo = {
+        mode = "todo",
         win = {
           position = "right",
-          size = 50,
+          size = 80,
           fixed = false,
           padding = { top = 0, left = 0 },
         },
       },
 
-      todo_right = {
-        mode = "todo",
+      lsp_incoming_calls = {
+        mode = "lsp_incoming_calls",
+        keys = {
+          ["<cr>"] = "jump_close",
+        },
         win = {
-          position = "right",
-          size = 50,
+          title = "Incoming Calls",
+          type = "float",
+          relative = "cursor",
+          border = "rounded",
+          position = { 0.03, 0.03 },
+          size = { width = 50, height = 10 },
           fixed = false,
-          padding = { top = 0, left = 0 },
-        }
+        },
+      },
+
+      lsp_outgoing_calls = {
+        mode = "lsp_outgoing_calls",
+        keys = {
+          ["<cr>"] = "jump_close",
+        },
+        win = {
+          title = "Outgoing Calls",
+          type = "float",
+          relative = "cursor",
+          border = "rounded",
+          position = { 0.03, 0.03 },
+          size = { width = 60, height = 10 },
+          fixed = false,
+        },
       },
     },
   },
 
   keys = {
-    -- {
-    --   "<leader>x",
-    --   desc = "Trouble",
-    -- },
     {
       "<leader>xx",
-      "<cmd>Trouble diagnostics toggle focus=true win.size=30<cr>",
-      desc = "Diagnostics (Trouble)",
+      "<cmd>Trouble diagnostics toggle focus=true<cr>",
+      desc = "Diagnostics",
     },
     {
       "<leader>xX",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xa",
-      "<cmd>Trouble all toggle focus=true<cr>",
-      desc = "Symbols (Trouble)",
+      "<cmd>Trouble diagnostics toggle focus=true filter.buf=0<cr>",
+      desc = "Diagnostics (Buffer)",
     },
     {
       "<leader>xo",
-      "<cmd>Trouble all toggle focus=true<cr>",
-      desc = "Symbols/Outline (Trouble)",
-    },
-    {
-      "<leader>xs",
-      "<cmd>Trouble diagnostics_right toggle focus=true<cr>",
-      desc = "Diagnostics (Trouble)",
+      "<cmd>Trouble s toggle focus=true<cr>",
+      desc = "Outline (Trouble)",
     },
     {
       "<leader>xt",
-      "<cmd>Trouble todo_right toggle focus=true<cr>",
-      desc = "Todo (Trouble)",
+      "<cmd>Trouble todo toggle focus=true<cr>",
+      desc = "Todo",
     },
     {
       "<leader>xl",
-      "<cmd>Trouble lsp toggle focus=true win.position=right win.size=50<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
+      "<cmd>Trouble lsp toggle focus=true win.position=right win.size=60<cr>",
+      desc = "LSP Definitions / references / ...",
     },
     {
       "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
+      "<cmd>Trouble loclist toggle focus=true win.position=right win.size=60<cr>",
+      desc = "Location List",
     },
     {
-      "<leader>xQ",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
+      "<leader>xq",
+      "<cmd>Trouble qflist toggle focus=true win.position=right win.size=60<cr>",
+      desc = "Quickfix List",
+    },
+
+    {
+      "<leader>xc",
+      "<cmd>Trouble lsp_incoming_calls toggle focus=true win.type=float<cr>",
+      desc = "Incoming Calls",
+    },
+
+    {
+      "<leader>xC",
+      "<cmd>Trouble lsp_outgoing_calls toggle focus=true win.type=float<cr>",
+      desc = "Outgoing Calls",
     },
   },
 }
