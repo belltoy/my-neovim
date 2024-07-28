3bb0dc0e8b041d20d4296acb7cdb323fdc0fcbfe
belltoy <belltoy@gmail.com>
2024-07-28 20:18:34 +0800

    Add live_grep_args extension for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    { 'nvim-telescope/telescope-live-grep-args.nvim', version = "^1.0.0" },
     'nvim-tree/nvim-web-devicons',
     'folke/todo-comments.nvim',
   },
@@ -107,6 +108,7 @@ return {
     })
 
     telescope.load_extension('fzf')
+    telescope.load_extension('live_grep_args')
     telescope.load_extension('projects')
 
     -- Keybindings
@@ -114,8 +116,9 @@ return {
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
     keymap.set("n", "<leader>fF", "<cmd>Telescope find_files no_ignore=true hidden=true no_ignore_parent=true<cr>", { desc = "Find files with no ignore/hidden" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Find recent files" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep_args<cr>", { desc = "Find string in cwd with args" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor" })
     keymap.set("n", "<leader>fP", "<cmd>lua require('telescope').extensions.projects.projects()<cr>", { desc = "Find project" })
     keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
